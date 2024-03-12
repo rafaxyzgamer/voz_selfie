eeb006c02528997df9eea0c6972eecedce4ef3db
rafaxyzgamer <131929400+rafaxyzgamer@users.noreply.github.com>
2024-03-11 21:41:36 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -24,3 +24,22 @@ var Content = event.results[0][0].transcript;
         speak();
       }
 }
+function speak(){
+  var synth = window.speechSynthesis;
+
+  speakData = "Tirando sua selfie em cinco segundos";
+  
+  var utterThis = new SpeechSynthesisUtterance(speakData);
+  
+  synth.speak(utterThis);
+
+  Webcam.attach(camera);
+}
+
+camera = document.getElementById("camera");
+Webcam.set({
+  width:360,
+  height:250,
+  image_format: 'jpeg',
+  jpeg_quality:90
+});
